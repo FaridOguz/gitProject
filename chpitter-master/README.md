62077d164a348e00ee970afa29dba49631535383
faridibrahimli <faridoguz92@gmail.com>
2016-06-20 22:44:21 +0400

    cpitter

---
+++ chpitter-master/README.md
@@ -0,0 +1,8 @@
+#Chpitter Project
+
+###instructions for students:
+
+1. Create vendors folder
+2. Add Bootstrap (Sass) to vendors folder
+3. Add jQuery to vendors folder
+4. Associated sass partials are in assets/sass/partials directory
---
+++ chpitter-master/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Chpitter</title>
+   <!-- <link rel="stylesheet" type="text/css" href="bootstrap/bootstrap.css"> -->
+   <link rel="stylesheet" href="assets/css/style.css" media="screen" title="no title" charset="utf-8">
+</head>
+
+<body>
+   <div class="container-fluid">
+
+      <!-- header section { -->
+        <div class="row">
+            <section id="header">
+                <div class="col-md-12">
+                    <div class="col-md-5">
+                      <ul class="nav nav-pills">
+                        <li role="presentation"><a href="#"><img  src="img/home.png"> Home</a></li>
+                        <li role="presentation"><a href="#"><img  src="img/bell1.png"> Notifications</a></li>
+                        <li role="presentation"><a href="#"><img src="img/message.png"> Messages</a></li>
+                      </ul>
+                    </div><!--col-5-->
+                    <div class="col-md-2"><img class="F_img1" src="img/twitter_icon1.png"></div>
+                    <div class="col-md-5">
+                      <!-- <div  class="col-lg-6"> -->
+    <div class="input-group">
+      <input id="search" type="text" class="form-control" placeholder="Search Twitter">
+      <span class="input-group-btn">
+        <!-- <button class="btn btn-default" type="button">Go!</button> -->        
+      </span>
+     
+    <img class="button" src="img/default_profile.png">
+    <button id="F_btn1" type="button" class="btn btn-primary">Twit</button>
+   </div>      
+      </div>      
+       <!-- </div> -->
+                </div> <!-- Farid + Eldar -->
+            </section>
+        </div>
+  
+      <!-- header section } -->
+
+      <!-- body section { -->
+        <div class="row">
+            <section class="col-md-10 col-md-offset-1" id="body">
+              <div id="profile-column" class="col-md-3"></div> <!-- Nurgul -->
+              <div id="body-column" class="col-md-6"></div> <!-- Iso -->
+              <div id="suggestion-column" class="col-md-3"></div> <!-- Mahtab -->
+            </section>
+        </div>
+      <!-- body section } -->
+
+    </div>
+</body>
+<script src="vendors/jQuery/jquery.js" charset="utf-8"></script>
+<script src="assets/scripts/app.js" charset="utf-8"></script>
+</html>
