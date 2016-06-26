79a74cd312c9a3b0d8284eec37bcc2e52d3bffda
faridibrahimli <faridoguz92@gmail.com>
2016-06-26 20:21:08 +0400

    06.23

---
+++ 06.23.2016/header/index.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Chpitter</title>
+    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.css" media="screen" title="no title"
+        charset="utf-8">
+    <!-- <link rel='shortcut icon' type='image/x-icon' href='images/favicon.ico'> -->
+    <link rel="stylesheet" href="assets/css/style.css" media="screen" title="no title"
+        charset="utf-8">
+    <link rel='stylesheet' type='text/css' href='assets/css/styles.css'>
+    <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/style.css" media="screen" title="no title"
+        charset="utf-8">
+</head>
+
+<body>
+    <div class="container-fluid">
+            <div class="e-header-wrap">
+                <section class="col-md-10 col-md-offset-1" id="header">
+                    <div class="col-md-5">
+                        <ul>
+                            <li>
+                                <span class='first Icon Icon--home'>
+                                    <span class="text-e">Home</span>
+                                </span>
+                            </li>
+                            <li>
+                                <span class='second Icon Icon--discover'>
+                                    <span class="text-e">Notifications</span>
+                                </span>
+                            </li>
+                            <li>
+                                <span class='second Icon Icon--message'>
+                                    <span class="text-e">Messages</span>
+                                </span>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="col-md-4 col-md-offset-4">
+                            <img class="e-bird" src="img/twitter-icon.png">
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 col-md-offset-1">
+                        <div class="header_right_side">
+
+                        </div>
+                        <div class="col-md-6">
+                            <input class="e-search" type="search" name="search" placeholder="Search Twitter ">
+                        </div>
+                        <div class="col-md-1">
+                            <div class="e-egg">
+                                <img class="" src="img/egg.png">
+                            </div>
+
+                        </div>
+                        <div class="col-md-4 ">
+                            <a class="tweetbtn">
+                                <span class='Icon Icon--tweet'></span>Tweet</a>
+                        </div>
+                    </div>
+                </section>
+            </div>
+
+    <section class="col-md-10 col-md-offset-1" id="body">
+      <div class="col-md-6 F_notifivations">  <!-- Farid -->
+          <h3>Notifications</h3>
+          <li><a>All</a><span> /</span></li>
+          <li><a>People you follow</a></li>
+          <hr>
+          <div class="col-md-12">
+            <img src="img/user-avatar.svg" alt="" class="F_imguser" />
+            <span><b>Turan Ibrahim</b> and 6 others followed you</span>
+            <span class="F_span5h">5h</span>
+          </div>
+          <div class="col-md-12">
+            <div class="F_img_user">
+                <img src="img/E.jpg" alt="" class="F_imgs"/>
+                <img src="img/M.jpg" alt="" class="F_imgs"/>
+                <img src="img/N.jpg" alt="" class="F_imgs"/>
+                <img src="img/I.jpg" alt="" class="F_imgs"/>
+                <img src="img/F.jpg" alt="" class="F_imgs"/>
+          </div>
+          <hr>
+        </div>
+        <div class="col-md-12">
+          <img src="img/user-avatar.svg" alt="" class="F_imguser" />
+          <span><b>Ann C. Curtiss</b> and 5 others followed you</span>
+          <span class="F_span5h">Jun 23</span>
+        </div>
+        <div class="col-md-12">
+          <div class="F_img_user">
+            <div class="F_a"></div>
+            <img src="img/N.jpg" alt="" class="F_imgs"/>
+            <img src="img/F.jpg" alt="" class="F_imgs"/>
+            <img src="img/I.jpg" alt="" class="F_imgs"/>
+            <img src="img/E.jpg" alt="" class="F_imgs"/>
+            <img src="img/M.jpg" alt="" class="F_imgs"/>
+        </div>
+        <hr>
+      </div>
+      <div class="col-md-12">
+        <img src="img/user-avatar.svg" alt="" class="F_imguser" />
+        <span><b>Barack Obama</b> and 5 others followed you</span>
+        <span class="F_span5h">Jun 15</span>
+      </div>
+      <div class="col-md-12">
+        <div class="F_img_user">
+          <img src="img/M.jpg" alt="" class="F_imgs"/>
+          <img src="img/I.jpg" alt="" class="F_imgs"/>
+          <img src="img/N.jpg" alt="" class="F_imgs"/>
+          <img src="img/F.jpg" alt="" class="F_imgs"/>
+          <img src="img/E.jpg" alt="" class="F_imgs"/>
+      </div>
+      <hr>
+    </div>
+    <div class="col-md-12">
+        <div class="col-md-5"></div>
+        <div class="col-md-2">
+              <img src="img/twitter.svg" alt="" class="F_twittersvg"/>
+        </div>
+        <div class="col-md-5"></div>
+    </div>
+      </div><!-- Farid -->
+
+    </section>
+    </div>
+    <!-- body section } -->
+
+    </div>
+</body>
+<script src="vendor/jQuery/jquery.js" charset="utf-8"></script>
+<script src="assets/scripts/app.js" charset="utf-8"></script>
+
+</html>
---
+++ 06.23.2016/triangle/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>cpitter triangle</title>
+<link rel="stylesheet" href="style.css" media="screen" title="no title" charset="utf-8">
+  </head>
+  <body>
+<div class="F_triangle"></div><!--ucbucaq-->
+<div class="F_drpdown">
+  <button type="button" name="button" class="F_drp_btn1"><p><b>Ismayil</b><br><span class="F_span">view profile</span</p></button><hr>
+  <!-- <button type="button" name="button" class="F_drp_btn"></button><hr> -->
+  <button type="button" name="button" class="F_drp_btn"><span class="F_span">Lists</span></button><hr>
+  <button type="button" name="button" class="F_drp_btn"><span class="F_span">Help</span></button>
+  <button type="button" name="button" class="F_drp_btn"><span class="F_span">Keyboard shortcuts</span></button><hr>
+  <button type="button" name="button" class="F_drp_btn"><span class="F_span">Settings</span></button>
+  <button type="button" name="button" class="F_drp_btn"><span class="F_span">Log out</span></button>
+
+
+
+  <!-- <ul>
+    <li><a href="#"><b>Ismayil</b><br/><a>view profile</a></li>
+    <hr>
+    <li> <a href="#">Lists</a></li>
+    <hr>
+    <li><a href="#">Help</a><br/><a href="#">Keyboard shortcuts</a></li>
+    <hr>
+    <li><a href="#">Setting</a><br><a href="#">Log out</a></li>
+
+  </ul> -->
+
+</div>
+  </body>
+</html>
